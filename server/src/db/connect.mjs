628ec127b0e53f7baf87b393afb1df4527808067
Roy Peled <peled.roy@gmail.com>
2021-09-12 18:46:14 +0300

    added DB env variable

--- server/src/db/connect.mjs
+++ server/src/db/connect.mjs
@@ -1,3 +1,5 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/fakestore', {useNewUrlParser: true, useUnifiedTopology: true});
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/fakestore';
+
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
